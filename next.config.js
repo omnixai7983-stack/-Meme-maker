7e50131d14bc28a3aa591d150deb1e1b7dafecf6
omnixai7983-stack <omnixai7983@gmail.com>
2025-10-07 09:26:02 -0400

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
